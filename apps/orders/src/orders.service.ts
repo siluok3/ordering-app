6764620ccc2e0f8e3f780f9ee855b2e8cffc4d48
Kiriakos Papachristou <koulispapas@gmail.com>
2023-12-15 17:26:27 +0100

    Setup connection between billing and orders service

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
   async createOrder(request: CreateOrderRequest) {
     return this.ordersRepository.create(request);
   }
